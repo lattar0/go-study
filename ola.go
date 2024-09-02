e430b870d188f8e97f363606c7ffda9dd326aac9
João Francisco <lattaro@tuta.io>
2024-09-02 18:46:34 -0300

    first project

---
+++ ola.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const prefixoOlaPortugues = "Olá, "
+
+func Ola(nome string) string {
+	if nome == "" {
+		nome = "mundo"
+	}
+
+	return prefixoOlaPortugues + nome
+}
+
+func main() {
+	fmt.Println(Ola("Chris"))
+}
